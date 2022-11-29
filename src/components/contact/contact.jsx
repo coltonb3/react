c4ca1b94de05858821a391d39593af1d2ef60adb
Colton Bush <colton@Coltons-Air.home>
2022-11-29 16:57:42 -0600

    portfolio in react

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './contact.css'
+
+
+const contact = () => {
+  return (
+   <section id='contact'>
+    Contact
+   </section>
+  )
+}
+ 
+export default contact
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './nav.css'
+import {RiHome7Line} from 'react-icons/ri'
+import {SlUser} from 'react-icons/sl'
+import {MdWorkOutline} from 'react-icons/md'
+import {AiOutlineMail} from 'react-icons/ai'
+import { useState} from 'react'
+
+
+ const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+  return (
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')}className={activeNav=== '#' ? 'active' : ''}><RiHome7Line/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')}className={activeNav=== 'about' ? 'active' : ''}><SlUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')}className={activeNav=== 'experience' ? 'active' : ''}><MdWorkOutline/></a>
+      <a href="#contact"onClick={() => setActiveNav('#contact')}className={activeNav=== 'contact' ? 'active' : ''}><AiOutlineMail/></a>
+    </nav>
+  
+  )
+}
+
+export default Nav 
+  
