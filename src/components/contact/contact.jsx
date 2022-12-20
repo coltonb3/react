a5c507c7c7ffa66ea0ca51cfaa8e5a4d0e9f718b
coltonb3 <clyb333@gmail.com>
2022-12-19 20:39:04 -0600

    react work

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,10 +5,11 @@ import './contact.css'
 const contact = () => {
   return (
    <section id='contact'>
+    <div className='contact'>
     <h3>Lets Talk!</h3>
-    <h5>Email</h5>
-    <h5>Call </h5>
-
+      <a href="mailto:clyb333@gmail.com">Email</a>
+      <a href="tel:+12172598112">Cell</a>
+    </div>
    </section>
   )
 }
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -3,9 +3,9 @@ import './experience.css'
 const experience = () => {
   return (
    <section id='experience'>
-     
+     <h4>Familiar Languages</h4>
+     <br/>
     <div className='icons'>
-    
    <i class="devicon-html5-plain"></i>
    <i class="devicon-css3-plain"></i>
    <i class="devicon-bootstrap-plain"></i>
@@ -13,10 +13,14 @@ const experience = () => {
    <i class="devicon-react-original"></i>
    <i class="devicon-javascript-plain"></i>
    <i class="devicon-nodejs-plain"></i>
-   <i class="devicon-python-plain"></i>
-            
+   <i class="devicon-python-plain"></i>    
     </div>
 
+  <div class="projects-container">
+    
+
+
+  </div>
 
 
    </section>
